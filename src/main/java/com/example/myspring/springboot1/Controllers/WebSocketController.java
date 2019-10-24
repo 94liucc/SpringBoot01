0fb40a5c31c5fe70c31d34be8b874efa03a16408
刘成 <liucheng@joycastle.mobi>
2019-10-24 11:41:34 +0800

    websocket

--- src/main/java/com/example/myspring/springboot1/Controllers/WebSocketController.java
+++ src/main/java/com/example/myspring/springboot1/Controllers/WebSocketController.java
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.socket.TextMessage;
